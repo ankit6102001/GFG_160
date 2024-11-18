db8834739df97585aa6c1d24f86adeb8ba207aab
Ankit Chaubey <72733314+ankit6102001@users.noreply.github.com>
2024-11-18 22:13:07 +0530

    Update Day-1-Second Largest.py

--- Day-1-Second Largest.py
+++ Day-1-Second Largest.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def getSecondLargest(self, arr):
+        # Code Here
+        if len(arr) < 2:
+            return -1  
+        
+        largest = second_largest = -1
+        
+        for num in arr:
+            if num > largest:
+                second_largest = largest
+                largest = num
+            elif num > second_largest and num < largest:
+                second_largest = num
+        
+        return second_largest if second_largest != -1 else -1
