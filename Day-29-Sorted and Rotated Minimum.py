5697608ce8cedb4c74f8eeccf309933f61fc98f2
Ankit Chaubey <72733314+ankit6102001@users.noreply.github.com>
2024-12-13 22:09:58 +0530

    Create Day-29-Sorted and Rotated Minimum.py

---
+++ Day-29-Sorted and Rotated Minimum.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def findMin(self, arr):
+        #complete the function here
+        left, right = 0, len(arr) - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if arr[mid] > arr[right]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return arr[left]
