2480cd8ae1302d53fb388e79c313a54ee7418cb2
Ankit Chaubey <72733314+ankit6102001@users.noreply.github.com>
2024-12-21 22:19:57 +0530

    Create Day-37-Rotate by 90 degree.py

---
+++ Day-37-Rotate by 90 degree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotateby90(self, mat): 
+        # code here
+        n = len(mat)
+        
+        for i in range(n):
+            mat[i].reverse()  
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+        
+        return
