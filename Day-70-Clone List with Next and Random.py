da7dc1586e787b668c08432e55010eec8b48bf13
Ankit Chaubey <72733314+ankit6102001@users.noreply.github.com>
2025-01-23 22:22:51 +0530

    Create Day-70-Clone List with Next and Random.py

---
+++ Day-70-Clone List with Next and Random.py
@@ -0,0 +1,34 @@
+# class Node:
+
+#     def __init__(self, x):
+#         self.data = x
+#         self.next = None
+#         self.random = None
+        
+class Solution:
+    def cloneLinkedList(self, head):
+        if not head:
+            return None
+
+        d = {}
+        ch = Node(head.data)
+        chh = ch
+        d[head] = ch
+
+        h = head.next
+        while h:
+            nn = Node(h.data)
+            chh.next = nn
+            d[h] = nn
+            h = h.next
+            chh = nn
+
+        h = head
+        chh = ch
+        while h:
+            if h.random:
+                chh.random = d[h.random]
+            h = h.next
+            chh = chh.next
+
+        return ch
