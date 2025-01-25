0c88b7a7711b496b7be0de9be9f389454439f78c
Ankit Chaubey <72733314+ankit6102001@users.noreply.github.com>
2025-01-25 23:42:17 +0530

    Create Day-72-Find the first node of loop in linked list.py

---
+++ Day-72-Find the first node of loop in linked list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findFirstNode(self, head):
+        #code here
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow, fast = slow.next, fast.next
+                return slow
+        return None
